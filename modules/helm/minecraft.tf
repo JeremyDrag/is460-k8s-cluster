9c91043119810317280d26b30a5aca701fa3c28d
Justin DeBo <ec2-user@ip-172-31-12-77.ec2.internal>
2024-03-29 20:45:05 +0000

    .

--- modules/helm/minecraft.tf
+++ modules/helm/minecraft.tf
@@ -7,26 +7,17 @@ resource "kubernetes_namespace" "minecraft" {
 resource "helm_release" "router" {
   name       = "mc-router"
   repository = "https://itzg.github.io/minecraft-server-charts/"
-  chart      = "itzg/mc-router"
+  chart      = "mc-router"
   namespace = kubernetes_namespace.minecraft.metadata[0].name
+}
 
-  #   values = [
-  #     "${file("values.yaml")}"
-  #   ]
-
-  #   set {
-  #     name  = "cluster.enabled"
-  #     value = "true"
-  #   }
-
-  #   set {
-  #     name  = "metrics.enabled"
-  #     value = "true"
-  #   }
-
-  #   set {
-  #     name  = "service.annotations.prometheus\\.io/port"
-  #     value = "9127"
-  #     type  = "string"
-  #   }
+resource "helm_release" "minecraft" {
+  name       = "mc-router"
+  repository = "https://itzg.github.io/minecraft-server-charts/"
+  chart      = "minecraft"
+  namespace = kubernetes_namespace.minecraft.metadata[0].name
+  set {
+    name = "minecraftServer.eula"
+    value = "true"
+  }
 }
